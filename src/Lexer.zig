32392d9c38a9913086e72518611329786612a7df
Johan-Mi <johanmi@protonmail.com>
2023-07-26 14:10:53 +0200

    Lex pipes

---
+++ src/Lexer.zig
@@ -0,0 +1,48 @@
+const std = @import("std");
+const ArrayList = std.ArrayList;
+
+const Self = @This();
+
+const Token = []const u8;
+
+source: []const u8,
+tokens: std.ArrayList(Token),
+
+pub fn lex(source: []const u8, allocator: std.mem.Allocator) !ArrayList(Token) {
+    var lexer = Self{ .source = source, .tokens = ArrayList(Token).init(allocator) };
+    return lexer.step();
+}
+
+fn step(self: *Self) !ArrayList(Token) {
+    self.chomp();
+    if (self.eat("|")) {
+        try self.tokens.append("|");
+    } else {
+        const word = self.takeNone(" |") orelse return self.tokens;
+        try self.tokens.append(word);
+    }
+    return @call(.always_tail, step, .{self});
+}
+
+fn chomp(self: *Self) void {
+    self.source = std.mem.trimLeft(u8, self.source, " ");
+}
+
+fn eat(self: *Self, str: []const u8) bool {
+    if (!std.mem.startsWith(u8, self.source, str)) {
+        return false;
+    }
+    self.source = self.source[str.len..];
+    return true;
+}
+
+fn takeNone(self: *Self, values: []const u8) ?[]const u8 {
+    const index = std.mem.indexOfAny(u8, self.source, values) orelse self.source.len;
+    if (index == 0) {
+        return null;
+    } else {
+        const slice = self.source[0..index];
+        self.source = self.source[index..];
+        return slice;
+    }
+}
--- src/main.zig
+++ src/main.zig
@@ -6,15 +6,6 @@ fn showPrompt() !void {
     try std.io.getStdOut().writeAll("> ");
 }
 
-fn lex(line: []const u8, allocator: Allocator) !ArrayList([]const u8) {
-    var token_iter = std.mem.tokenizeScalar(u8, line, ' ');
-    var tokens = ArrayList([]const u8).init(allocator);
-    while (token_iter.next()) |token| {
-        try tokens.append(token);
-    }
-    return tokens;
-}
-
 fn interpret(tokens: []const []const u8, allocator: Allocator) !void {
     if (tokens.len != 0) {
         var child = std.process.Child.init(tokens, allocator);
@@ -38,7 +29,7 @@ pub fn main() !void {
         defer line.deinit();
         std.io.getStdIn().reader().streamUntilDelimiter(line.writer(), '\n', null) catch return;
 
-        var tokens = try lex(line.items, allocator);
+        var tokens = try @import("Lexer.zig").lex(line.items, allocator);
         defer tokens.deinit();
 
         try interpret(tokens.items, allocator);
